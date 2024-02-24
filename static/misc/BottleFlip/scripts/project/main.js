0a73568b21a309136a3ee8dfba5498d782998726
rednotsus <Jiayangc1@gmail.com>
2024-02-24 10:46:36 -0600

    fix bottleflip 3d

---
+++ static/misc/BottleFlip/scripts/project/main.js
@@ -0,0 +1,27 @@
+
+// Import any other script files here, e.g.:
+// import * as myModule from "./mymodule.js";
+
+runOnStartup(async runtime =>
+{
+	// Code to run on the loading screen.
+	// Note layouts, objects etc. are not yet available.
+	
+	runtime.addEventListener("beforeprojectstart", () => OnBeforeProjectStart(runtime));
+});
+
+async function OnBeforeProjectStart(runtime)
+{
+	// Code to run just before 'On start of layout' on
+	// the first layout. Loading has finished and initial
+	// instances are created and available to use here.
+	
+	runtime.addEventListener("tick", () => Tick(runtime));
+}
+
+function Tick(runtime)
+{
+	// Code to run every tick
+}
+
+
---
+++ static/misc/BottleFlip/scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async Generalsheet_Event11_Act1(runtime, localVars)
+	{
+		localVars.vv = JSONString;
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
