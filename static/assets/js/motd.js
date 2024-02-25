331f28dacc7eb511c7d7357d7a8956623edb7931
22yeets22 <132288501+22yeets22@users.noreply.github.com>
2024-02-25 10:49:49 -0600

    Create motd.js

---
+++ static/assets/js/motd.js
@@ -0,0 +1,17 @@
+fetch("./assets/motd.json")
+  .then((response) => response.json())
+  .then((data) => {
+    document.getElementById("motd").onclick = function() {
+      Swal.fire({icon: 'info', title: 'Message Of The Day', html: data.motd.body, footer: `<i style='font-size: 11px;'>${data.qotd.footer}</i>`});
+    }
+    document.getElementById("qotd").onclick = function() {
+      Swal.fire({icon: 'info', title: 'Quote Of The Day', html: data.qotd.body, footer: `<i style='font-size: 11px;'>${data.qotd.footer}</i>`});
+    }
+  }).catch((error) => {
+    document.getElementById("motd").onclick = function() {
+      Swal.fire({icon: 'error', title: 'Error', html: 'Well oh no', footer: `<i style='font-size: 11px;'>${error}</i>`});
+    }
+    document.getElementById("qotd").onclick = function() {
+      Swal.fire({icon: 'error', title: 'Error', html: 'Well oh no', footer: `<i style='font-size: 11px;'>${error}</i>`});
+    }
+  });
