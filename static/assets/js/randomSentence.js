73c0cbf6b123276b929d92f618f268152bc9a31c
22yeets22 <132288501+22yeets22@users.noreply.github.com>
2024-02-26 21:02:12 -0600

    onload

--- static/assets/js/randomSentence.js
+++ static/assets/js/randomSentence.js
@@ -1,20 +1,21 @@
-fetch("./assets/quotes.json")
-  .then((response) => response.json())
-  .then((data) => {
-    const totalFrequency = data.reduce((acc, item) => acc + item.frequency, 0);
-    const rng = Math.random() * totalFrequency;
+window.addEventListener("load", (event) => {
+  fetch("/assets/quotes.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const totalFrequency = data.reduce((acc, item) => acc + item.frequency, 0);
+      const rng = Math.random() * totalFrequency;
 
-    let cumulativeFrequency = 0;
-    for (const item of data) {
-      cumulativeFrequency += item.frequency;
-      if (rng < cumulativeFrequency) {
-        document.getElementById("sub").innerHTML =
-          item.msg || "nothing to see here<br>no quotes";
-        break;
+      let cumulativeFrequency = 0;
+      for (const item of data) {
+        cumulativeFrequency += item.frequency;
+        if (rng < cumulativeFrequency) {
+          document.getElementById("sub").innerHTML =
+            item.msg || "nothing to see here<br>no quotes";
+          break;
+        }
       }
-    }
-  })
-  .catch((error) => {
-    document.getElementById("sub").innerHTML =
-      "idk what happened something broke";
-  });
+    })
+    .catch((error) => {
+      document.getElementById("sub").innerHTML = "idk what happened something broke";
+    });
+});
