b7f2328b4c6f72e3d839f72b76d9741a96d72494
rednotsus <Jiayangc1@gmail.com>
2024-02-26 21:19:00 -0600

    ad improvemtns
    ad

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -9,33 +9,46 @@ function script(text) {
 // ====================================
 // SCRIPT INJECTION
 // ====================================
-const adscipterz92 = document.createElement("script");
-adscipterz92.setAttribute("async", "");
-adscipterz92.setAttribute(
+fetch("/assets/ads.json")
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.domains.includes(window.location.hostname)) {
+      const adscipterz92 = document.createElement("script");
+      adscipterz92.setAttribute("async", "");
+      adscipterz92.setAttribute(
+        "src",
+        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317"
+      );
+      adscipterz92.setAttribute("crossorigin", "anonymous");
+      document.head.append(adscipterz92);
+      script("Injected script 1/3 (Ad stuff)");
+    }
+  });
+const gascript = document.createElement("script");
+gascript.setAttribute("async", "");
+gascript.setAttribute(
   "src",
-  "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317"
+  "https://www.googletagmanager.com/gtag/js?id=G-N0LG27M8L8"
 );
-adscipterz92.setAttribute("crossorigin", "anonymous");
-document.head.append(adscipterz92);
-script("Injected script 1/2 (Ad stuff)");
-var gaenabled = window.localStorage.getItem("ga");
-if (gaenabled == "false") {
-  script("Skipped GA injection because it is disabled by the user.");
-} else {
-  const gascript = document.createElement("script");
-  gascript.setAttribute("async", "");
-  gascript.setAttribute(
-    "src",
-    "https://www.googletagmanager.com/gtag/js?id=G-N0LG27M8L8"
-  );
-  const inlinegascript = document.createElement("script");
-  inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
+const inlinegascript = document.createElement("script");
+inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
       gtag('config', 'G-N0LG27M8L8');`;
-  document.head.append(gascript, inlinegascript);
-  script("Injected script 2/2");
-}
+document.head.append(gascript, inlinegascript);
+script("Injected script 2/3");
+
+(function (ovu) {
+  var d = document,
+    s = d.createElement("script"),
+    l = d.scripts[d.scripts.length - 1];
+  s.settings = ovu || {};
+  s.src =
+    "//frighteningship.com/akW.5/wFYNW/dAlmQu2d9okIZZTM9Y6-bS2/5jleSeWvQR9iNnTqEFwGMoDjcyxANYiU0V1/MuTQA/whNJzxEu3Y";
+  s.async = true;
+  l.parentNode.insertBefore(s, l);
+})({});
+script("Injected script 3/3 (More Ad stuff)");
 
 // Collect Tab Cloak data from local storage
 var tab = localStorage.getItem("tab");
@@ -55,7 +68,7 @@ if (tabData.title) {
   document.title = tabData.title;
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Set the Tab icon if the Tab cloak data is there
   if (tabData.icon) {
     var iconLink = document.querySelector('link[rel="icon"]');
