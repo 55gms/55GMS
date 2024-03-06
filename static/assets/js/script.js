b2df87ac3bbb54f70e5e6df799ad04918f14994e
rednotsus <Jiayangc1@gmail.com>
2024-03-05 19:50:54 -0600

    Qol for Ads

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -9,21 +9,6 @@ function script(text) {
 // ====================================
 // SCRIPT INJECTION
 // ====================================
-fetch("/assets/ads.json")
-  .then((response) => response.json())
-  .then((data) => {
-    if (data.domains.includes(window.location.hostname)) {
-      const adscipterz92 = document.createElement("script");
-      adscipterz92.setAttribute("async", "");
-      adscipterz92.setAttribute(
-        "src",
-        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317"
-      );
-      adscipterz92.setAttribute("crossorigin", "anonymous");
-      document.head.append(adscipterz92);
-      script("Injected script 1/3 (Ad stuff)");
-    }
-  });
 const gascript = document.createElement("script");
 gascript.setAttribute("async", "");
 gascript.setAttribute(
@@ -36,7 +21,7 @@ inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
       gtag('js', new Date());
       gtag('config', 'G-N0LG27M8L8');`;
 document.head.append(gascript, inlinegascript);
-script("Injected script 2/3");
+script("Injected script 1/3");
 
 (function (d, z, s) {
   s.src = "https://" + d + "/400/" + z;
@@ -44,7 +29,7 @@ script("Injected script 2/3");
     (document.body || document.documentElement).appendChild(s);
   } catch (e) {}
 })("soumehoo.net", 7171452, document.createElement("script"));
-script("Injected script 3/3 (More Ad stuff)");
+script("Injected script 2/3 (More Ad stuff)");
 
 var tab = localStorage.getItem("tab");
 if (tab) {
@@ -74,3 +59,21 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 });
+
+fetch("/assets/ads.json")
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.domains.includes(window.location.hostname)) {
+      const adscipterz92 = document.createElement("script");
+      adscipterz92.setAttribute("async", "");
+      adscipterz92.setAttribute(
+        "src",
+        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317"
+      );
+      adscipterz92.setAttribute("crossorigin", "anonymous");
+      document.head.append(adscipterz92);
+      script("Injected script 3/3 (Adsense)");
+    } else {
+      console.log("Skipping Adsense Injection for this domain.");
+    }
+  });
