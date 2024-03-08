1c6e8631fd6dd0e632771e83db66f0298cf99581
rednotsus <Jiayangc1@gmail.com>
2024-03-07 20:58:13 -0600

    i guess monetag doesnt like us

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -2,7 +2,7 @@ function script(text) {
   console.log(
     "%cScript Injection",
     "color: cyan; font-weight: 600; background: black; padding: 0 5px; border-radius: 5px",
-    text,
+    text
   );
 }
 
@@ -13,7 +13,7 @@ const gascript = document.createElement("script");
 gascript.setAttribute("async", "");
 gascript.setAttribute(
   "src",
-  "https://www.googletagmanager.com/gtag/js?id=G-N0LG27M8L8",
+  "https://www.googletagmanager.com/gtag/js?id=G-N0LG27M8L8"
 );
 const inlinegascript = document.createElement("script");
 inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
@@ -23,12 +23,17 @@ inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
 document.head.append(gascript, inlinegascript);
 script("Injected script 1/3");
 
-(function (d, z, s) {
-  s.src = "https://" + d + "/400/" + z;
-  try {
-    (document.body || document.documentElement).appendChild(s);
-  } catch (e) {}
-})("soumehoo.net", 7171452, document.createElement("script"));
+(function (ovu) {
+  var d = document,
+    s = d.createElement("script"),
+    l = d.scripts[d.scripts.length - 1];
+  s.settings = ovu || {};
+  s.src =
+    "//frighteningship.com/akW.5/wFYNW/dAlmQu2d9okIZZTM9Y6-bS2/5jleSeWvQR9iNnTqEFwGMoDjcyxANYiU0V1/MuTQA/whNJzxEu3Y";
+  s.async = true;
+  l.parentNode.insertBefore(s, l);
+})({});
+
 script("Injected script 2/3 (More Ad stuff)");
 
 var tab = localStorage.getItem("tab");
@@ -68,7 +73,7 @@ fetch("/assets/ads.json")
       adscipterz92.setAttribute("async", "");
       adscipterz92.setAttribute(
         "src",
-        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317",
+        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317"
       );
       adscipterz92.setAttribute("crossorigin", "anonymous");
       document.head.append(adscipterz92);
