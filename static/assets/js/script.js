f7ff64a245f64c1528adf9b65f92853146f7e994
rednotsus <Jiayangc1@gmail.com>
2024-03-11 18:06:12 -0500

    unblockable ads

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -2,7 +2,7 @@ function script(text) {
   console.log(
     "%cScript Injection",
     "color: cyan; font-weight: 600; background: black; padding: 0 5px; border-radius: 5px",
-    text,
+    text
   );
 }
 
@@ -13,7 +13,7 @@ const gascript = document.createElement("script");
 gascript.setAttribute("async", "");
 gascript.setAttribute(
   "src",
-  "https://www.googletagmanager.com/gtag/js?id=G-N0LG27M8L8",
+  "https://www.googletagmanager.com/gtag/js?id=G-N0LG27M8L8"
 );
 const inlinegascript = document.createElement("script");
 inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
@@ -28,8 +28,7 @@ script("Injected script 1/3");
     s = d.createElement("script"),
     l = d.scripts[d.scripts.length - 1];
   s.settings = ovu || {};
-  s.src =
-    "//frighteningship.com/akW.5/wFYNW/dAlmQu2d9okIZZTM9Y6-bS2/5jleSeWvQR9iNnTqEFwGMoDjcyxANYiU0V1/MuTQA/whNJzxEu3Y";
+  s.src = "/assets/js/ads.js";
   s.async = true;
   l.parentNode.insertBefore(s, l);
 })({});
@@ -73,7 +72,7 @@ fetch("/assets/ads.json")
       adscipterz92.setAttribute("async", "");
       adscipterz92.setAttribute(
         "src",
-        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317",
+        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317"
       );
       adscipterz92.setAttribute("crossorigin", "anonymous");
       document.head.append(adscipterz92);
