bf3cb9e8e6685859bd9b83e0a71ebb0fb8fc8505
rednotsus <Jiayangc1@gmail.com>
2024-03-25 10:09:03 -0500

    update ads

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -2,7 +2,7 @@ function script(text) {
   console.log(
     "%cScript Injection",
     "color: cyan; font-weight: 600; background: black; padding: 0 5px; border-radius: 5px",
-    text,
+    text
   );
 }
 
@@ -13,7 +13,7 @@ const gascript = document.createElement("script");
 gascript.setAttribute("async", "");
 gascript.setAttribute(
   "src",
-  "https://www.googletagmanager.com/gtag/js?id=G-N0LG27M8L8",
+  "https://www.googletagmanager.com/gtag/js?id=G-N0LG27M8L8"
 );
 const inlinegascript = document.createElement("script");
 inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
@@ -23,13 +23,13 @@ inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
 document.head.append(gascript, inlinegascript);
 script("Injected script 1/3");
 
-(function (icl) {
+(function (mtxb) {
   var d = document,
     s = d.createElement("script"),
     l = d.scripts[d.scripts.length - 1];
-  s.settings = icl || {};
+  s.settings = mtxb || {};
   s.src =
-    "//this-park.com/aDW.5kwOYlW/dolKQR2D9sknZGTm9_6ybE2p5elvSYW/Qm9dNTT/E/wAM/Dgc/xZNaix0s1hMcTcA/w/N-zAEb3M";
+    "//glum-mortgage.com/alWe5.wdYEWLdrl/QB2S9kkNZ/TF9J6pbA2c5ulLS/W/Qh9/N/TpEHwzM-DncPxaNZiR0p1/MTTnA/w/NCzeEb3J";
   s.async = true;
   l.parentNode.insertBefore(s, l);
 })({});
@@ -73,7 +73,7 @@ fetch("/assets/static/ads.json")
       adscipterz92.setAttribute("async", "");
       adscipterz92.setAttribute(
         "src",
-        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317",
+        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317"
       );
       adscipterz92.setAttribute("crossorigin", "anonymous");
       document.head.append(adscipterz92);
