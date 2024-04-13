102e6b494d583b5fc241021d64c8d98a6192a762
rednotsus <Jiayangc1@gmail.com>
2024-04-12 22:32:38 -0500

    fix youtube

--- static/assets/js/apps.js
+++ static/assets/js/apps.js
@@ -4,17 +4,28 @@ window.addEventListener("load", (event) => {
     .then((response) => response.json())
     .then((apps) => {
       apps.sort((a, b) => a.name.localeCompare(b.name));
-      apps.forEach(function (game) {
-        let gameHtml;
-        gameHtml = `<div class="card" style="padding-top: 5px">
-          <a onclick="hire('${game.url}');">
-            <div class="image-container">
-              <img loading="lazy" src="${game.image}" style="border-radius: 25px">
-              <p class="item-name">${game.name}</p>
-            </div>
-          </a>
-        </div>`;
-        gameContainer.insertAdjacentHTML("beforeend", gameHtml);
+      apps.forEach(function (app) {
+        let appHtml;
+        if (app.url === "https://youtube.com/") {
+          appHtml = `<div class="card" style="padding-top: 5px">
+            <a onclick="alert('Youtube is not working, use Invidious (the other youtube) instead');">
+              <div class="image-container">
+                <img loading="lazy" src="${app.image}" style="border-radius: 25px">
+                <p class="item-name">${app.name}</p>
+              </div>
+            </a>
+          </div>`;
+        } else {
+          appHtml = `<div class="card" style="padding-top: 5px">
+            <a onclick="hire('${app.url}');">
+              <div class="image-container">
+                <img loading="lazy" src="${app.image}" style="border-radius: 25px">
+                <p class="item-name">${app.name}</p>
+              </div>
+            </a>
+          </div>`;
+        }
+        gameContainer.insertAdjacentHTML("beforeend", appHtml);
       });
 
       let searchbar = document.getElementById("searchbar");
