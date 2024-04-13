3a4e3a3477cecd38c5b5eefe1ce4bb782add2b2a
rednotsus <Jiayangc1@gmail.com>
2024-04-13 11:43:28 -0500

    fix cloaking

--- static/assets/js/settings.js
+++ static/assets/js/settings.js
@@ -158,3 +158,56 @@ function setPanicLink() {
   var link = document.getElementById("link").value;
   localStorage.setItem("PanicLink", link);
 }
+
+function cloak() {
+  let inFrame;
+
+  try {
+    inFrame = window !== top;
+  } catch (e) {
+    inFrame = true;
+  }
+
+  if (!inFrame && !navigator.userAgent.includes("Firefox")) {
+    const popup = open("about:blank", "_blank");
+    if (!popup || popup.closed) {
+      alert("Please allow popups and redirects.");
+    } else {
+      const doc = popup.document;
+      const iframe = doc.createElement("iframe");
+      const style = iframe.style;
+      const link = doc.createElement("link");
+
+      const name = tabData.title || "Dashboard";
+      const icon = tabData.icon || "/img/canvas.ico";
+
+      doc.title = name;
+      link.rel = "icon";
+      link.href = icon;
+
+      iframe.src = location.href;
+      style.position = "fixed";
+      style.top = style.bottom = style.left = style.right = 0;
+      style.border = style.outline = "none";
+      style.width = style.height = "100%";
+
+      doc.head.appendChild(link);
+      doc.body.appendChild(iframe);
+
+      const pLink =
+        localStorage.getItem(encodeURI("pLink")) ||
+        "https://canvas.houstonisd.org/";
+      location.replace(pLink);
+
+      const script = doc.createElement("script");
+      script.textContent = `
+        window.onbeforeunload = function (event) {
+          const confirmationMessage = 'Leave Site?';
+          (event || window.event).returnValue = confirmationMessage;
+          return confirmationMessage;
+        };
+      `;
+      doc.head.appendChild(script);
+    }
+  }
+}
