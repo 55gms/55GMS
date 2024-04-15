f3cc4347fffd05258d2e4a82a3ce55a46598baaa
22yeets22 <132288501+22yeets22@users.noreply.github.com>
2024-04-15 15:46:42 -0500

    placeholder not done

--- static/assets/js/games.js
+++ static/assets/js/games.js
@@ -1,35 +1,39 @@
 window.addEventListener("load", (event) => {
   const gameContainer = document.getElementById("game-container");
-  fetch("/assets/json/load/games.json")
-    .then((response) => response.json())
-    .then((games) => {
-      games.sort((a, b) => a.name.localeCompare(b.name));
-      games.forEach(function (game) {
-        let gameHtml;
-        if (game.usesProxy) {
-          gameHtml = `<div class="card" style="padding-top: 5px">
-          <a onclick="${game.alert ? `alert('${game.alert}'); ` : ""}hire('${game.url}');">
-            <div class="image-container">
-              <img loading="lazy" src="${game.image}" style="border-radius: 25px">
-              <p class="item-name">${game.name}</p>
-            </div>
-          </a>
-        </div>`;
-        } else {
-          gameHtml = `<div class="card" style="padding-top: 5px">
-            <a href="${game.url}" rel="noopener noreferrer" ${game.alert ? `onclick="alert('${game.alert}');"` : ""}>
-            <div class="image-container">
-              <img loading="lazy" src="${game.image}" style="border-radius: 25px">
-              <p class="item-name">${game.name}</p>
-            </div>
-          </a>
+  try {
+    fetch("/assets/json/load/games.json")
+      .then((response) => response.json())
+      .then((games) => {
+        games.sort((a, b) => a.name.localeCompare(b.name));
+        games.forEach(function (game) {
+          let gameHtml;
+          if (game.usesProxy) {
+            gameHtml = `<div class="card" style="padding-top: 5px">
+            <a onclick="${game.alert ? `alert('${game.alert}'); ` : ""}hire('${game.url}');">
+              <div class="image-container">
+                <img loading="lazy" src="${game.image}" style="border-radius: 25px">
+                <p class="item-name">${game.name}</p>
+              </div>
+            </a>
           </div>`;
-        }
-        gameContainer.insertAdjacentHTML("beforeend", gameHtml);
-      });
-
-      let searchbar = document.getElementById("searchbar");
-      if (searchbar)
-        searchbar.placeholder = `Click here or type to search through our ${games.length} games!`;
+          } else {
+            gameHtml = `<div class="card" style="padding-top: 5px">
+              <a href="${game.url}" rel="noopener noreferrer" ${game.alert ? `onclick="alert('${game.alert}');"` : ""}>
+              <div class="image-container">
+                <img loading="lazy" src="${game.image}" style="border-radius: 25px">
+                <p class="item-name">${game.name}</p>
+              </div>
+            </a>
+            </div>`;
+          }
+          gameContainer.insertAdjacentHTML("beforeend", gameHtml);
+        });
+  
+        let searchbar = document.getElementById("searchbar");
+        if (searchbar)
+          searchbar.placeholder = `Click here or type to search through our ${games.length} games!`;
+      } catch (error) {
+            console.error(error);
+            }
     });
 });
