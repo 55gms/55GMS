9924baf362a260ba9a4117592578be5da77b6765
RedNotSus <RedNotSus@users.noreply.github.com>
2024-07-14 02:38:21 +0000

    Prettified Code!

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -2,7 +2,7 @@ function script(text) {
   console.log(
     "%cScript Injection",
     "color: cyan; font-weight: 600; background: black; padding: 0 5px; border-radius: 5px",
-    text
+    text,
   );
 }
 
@@ -55,7 +55,7 @@ fetch("/assets/json/ads.json")
       adscipterz92.setAttribute("async", "");
       adscipterz92.setAttribute(
         "src",
-        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317"
+        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317",
       );
       adscipterz92.setAttribute("crossorigin", "anonymous");
       document.head.append(adscipterz92);
