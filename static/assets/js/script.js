c03b70eb24a6ddd2d1d1903fd3a5e97d22f29332
rednotsus <Jiayangc1@gmail.com>
2024-07-13 22:01:16 -0500

    opps

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -2,7 +2,7 @@ function script(text) {
   console.log(
     "%cScript Injection",
     "color: cyan; font-weight: 600; background: black; padding: 0 5px; border-radius: 5px",
-    text,
+    text
   );
 }
 
@@ -11,7 +11,7 @@ function script(text) {
 // ====================================
 const newScript = document.createElement("script");
 newScript.setAttribute("defer", "");
-newScript.setAttribute("data-domain", window.location);
+newScript.setAttribute("data-domain", "55gms.com");
 newScript.setAttribute("src", "https://data.ch3n.cc/js/script.js");
 document.head.append(newScript);
 script("Injected script 1/3");
@@ -55,7 +55,7 @@ fetch("/assets/json/ads.json")
       adscipterz92.setAttribute("async", "");
       adscipterz92.setAttribute(
         "src",
-        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317",
+        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317"
       );
       adscipterz92.setAttribute("crossorigin", "anonymous");
       document.head.append(adscipterz92);
