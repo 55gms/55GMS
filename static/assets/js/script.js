54527e18f454f34e4a95366d41191fc9555d0dd8
RedNotSus <RedNotSus@users.noreply.github.com>
2024-07-17 17:33:10 +0000

    Prettified Code!

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -2,7 +2,7 @@ function script(text) {
   console.log(
     "%cScript Injection",
     "color: cyan; font-weight: 600; background: black; padding: 0 5px; border-radius: 5px",
-    text
+    text,
   );
 }
 
@@ -14,7 +14,7 @@ newScript.setAttribute("defer", "");
 newScript.setAttribute("data-domain", "55gms.com");
 newScript.setAttribute(
   "src",
-  "https://data.ch3n.cc/js/script.tagged-events.js"
+  "https://data.ch3n.cc/js/script.tagged-events.js",
 );
 document.head.append(newScript);
 script("Injected script 1/3");
@@ -63,7 +63,7 @@ fetch("/assets/json/ads.json")
       adscipterz92.setAttribute("async", "");
       adscipterz92.setAttribute(
         "src",
-        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317"
+        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317",
       );
       adscipterz92.setAttribute("crossorigin", "anonymous");
       document.head.append(adscipterz92);
