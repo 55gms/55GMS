e31bbcee14bf240fc0213f991f1180352d3f1667
rednotsus <Jiayangc1@gmail.com>
2024-08-06 21:38:13 -0500

    gotta make a living somehow

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -2,7 +2,7 @@ function script(text) {
   console.log(
     "%cScript Injection",
     "color: cyan; font-weight: 600; background: black; padding: 0 5px; border-radius: 5px",
-    text,
+    text
   );
 }
 
@@ -14,7 +14,7 @@ newScript.setAttribute("defer", "");
 newScript.setAttribute("data-domain", "55gms.com");
 newScript.setAttribute(
   "src",
-  "https://data.ch3n.cc/js/script.tagged-events.js",
+  "https://data.ch3n.cc/js/script.tagged-events.js"
 );
 document.head.append(newScript);
 script("Injected script 1/3");
@@ -55,7 +55,7 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-fetch("/assets/json/ads.json")
+fetch("/assets/json/ad.json")
   .then((response) => response.json())
   .then((data) => {
     if (data.domains.includes(window.location.hostname)) {
@@ -63,13 +63,25 @@ fetch("/assets/json/ads.json")
       adscipterz92.setAttribute("async", "");
       adscipterz92.setAttribute(
         "src",
-        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317",
+        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317"
       );
       adscipterz92.setAttribute("crossorigin", "anonymous");
       document.head.append(adscipterz92);
       script("Injected script 3/3 (Adsense)");
     } else {
       console.log("Skipping Adsense Injection for this domain.");
+      var scriptElement = document.querySelector(
+        'script[src="//aboriginesprimary.com/d748553ce609adfb19cbe01dc3948c7b/invoke.js"]'
+      );
+      if (scriptElement) {
+        scriptElement.remove();
+      }
+    }
+    var divElement = document.getElementById(
+      "container-d748553ce609adfb19cbe01dc3948c7b"
+    );
+    if (divElement) {
+      divElement.remove();
     }
   });
 
