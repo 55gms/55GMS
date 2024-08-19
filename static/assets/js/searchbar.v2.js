8fa3a93c2e1fa56080f5e32b715ceec948f3e5a1
22yeets22 <132288501+22yeets22@users.noreply.github.com>
2024-08-19 12:12:17 -0500

    oooh search bar is cool

--- static/assets/js/searchbar.v2.js
+++ static/assets/js/searchbar.v2.js
@@ -1,10 +1,23 @@
 function search() {
-  let e = document.getElementById("searchbar").value.toLowerCase();
-  let n = document.getElementsByClassName("card");
-  for (let t = 0; t < n.length; t++)
-    n[t].innerHTML.toLowerCase().includes(e)
-      ? (n[t].style.display = "block")
-      : (n[t].style.display = "none");
+  let searchbarVal = document.getElementById("searchbar").value.toLowerCase();
+  let gameNames = document.getElementsByClassName("card");
+  let results = 0;
+  for (let t = 0; t < gameNames.length; t++) {
+    if (gameNames[t].innerText.toLowerCase().includes(searchbarVal)) {
+      gameNames[t].style.display = "block";
+      results++;
+    } else {
+      gameNames[t].style.display = "none";
+    }
+  }
+  let noResultsElement = document.getElementById("no-results");
+  if (noResultsElement) {
+    if (results == 0) {
+      noResultsElement.style.display = "block";
+    } else {
+      noResultsElement.style.display = "none";
+    }
+  }
 }
 
 document.addEventListener("keydown", function (e) {
