1e119e8a36652542b81211833f0e54efc92c4ae7
rednotsus <Jiayangc1@gmail.com>
2024-09-03 20:56:08 -0500

    add new feature

--- static/assets/js/media.js
+++ static/assets/js/media.js
@@ -4,7 +4,7 @@ function fetchTmdbId() {
   let poster;
   let encodedSearch = encodeURIComponent(search);
   let url =
-    "https://api.themoviedb.org/3/search/multi?api_key=66ca93aa37686b7a47476585271855c6&language=en-US&query=" +
+    "https://api.themoviedb.org/3/search/multi?api_key=d93115754010beb32ae8956c26dbc590&language=en-US&query=" +
     encodedSearch +
     "&page=1&include_adult=false";
   const gameContainer = document.getElementById("game-container");
@@ -72,13 +72,76 @@ function promptForSeasonAndEpisode(videoId) {
   hire(link);
 }
 
+function fetchPopular() {
+  url = `https://api.themoviedb.org/3/trending/all/week?api_key=d93115754010beb32ae8956c26dbc590`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      date = new Date();
+      const results = data.results;
+
+      localStorage.setItem("popular", JSON.stringify(results));
+      localStorage.setItem("popularDate", date);
+    });
+}
+
+function displayPopular() {
+  if (!localStorage.getItem("popular")) {
+    fetchPopular();
+  }
+  let popular = JSON.parse(localStorage.getItem("popular"));
+  let lastFetchDate = new Date(localStorage.getItem("popularDate"));
+  let currentDate = new Date();
+  let timeDifferenceInDays =
+    Math.abs(currentDate - lastFetchDate) / (1000 * 60 * 60 * 24);
+
+  if (timeDifferenceInDays > 7) {
+    fetchPopular();
+  }
+  const gameContainer = document.getElementById("game-container");
+  gameContainer.innerHTML = "";
+  popular.forEach(function (movie) {
+    let poster;
+    if (movie.poster_path === null || !movie.poster_path) {
+      poster = "/img/no-media.svg";
+    } else {
+      poster = "https://image.tmdb.org/t/p/w500/" + movie.poster_path;
+    }
+
+    let gameHtml;
+    if (movie.media_type === "tv") {
+      gameHtml = `<div class="card" style="padding-top: 5px">
+        <a onclick="promptForSeasonAndEpisode(${movie.id})"> 
+          <div class="image-container">
+            <img loading="eager" src="${poster}" style="border-radius: 25px">
+            <p class="item-name">${movie.name || movie.title}</p> 
+          </div>
+        </a>
+      </div>`;
+    } else if (movie.media_type === "movie") {
+      link = `https://vidsrc.rip/embed/movie/${movie.id}`;
+      gameHtml = `<div class="card" style="padding-top: 5px">
+        <a onclick="hire('${link}');"> 
+          <div class="image-container">
+            <img loading="eager" src="${poster}" style="border-radius: 25px">
+            <p class="item-name">${movie.name || movie.title}</p> 
+          </div>
+        </a>
+      </div>`;
+    }
+    gameContainer.insertAdjacentHTML("beforeend", gameHtml);
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   let cooldown = false;
-
+  let popularText = document.getElementById("popular");
   document
     .getElementById("searchbar")
     .addEventListener("keypress", function (e) {
       if (e.key === "Enter") {
+        popularText.style.display = "none";
         if (cooldown) {
           document.getElementById("cooldownNotice").style.display = "block";
         } else {
@@ -88,8 +151,9 @@ document.addEventListener("DOMContentLoaded", function () {
           setTimeout(function () {
             cooldown = false;
             document.getElementById("cooldownNotice").style.display = "none";
-          }, 3000);
+          }, 2000);
         }
       }
     });
+  displayPopular();
 });
