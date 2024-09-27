318c73c1f1fc21be8165ff7cf8cc6d4d9c614537
rednotsus <Jiayangc1@gmail.com>
2024-09-26 22:33:39 -0500

    remov ethis

--- index.js
+++ index.js
@@ -17,15 +17,12 @@ gemma2-9b-it
 llama-3.1-70b-versatile
 llama-3.1-8b-instant
 llama-3.2-11b-text-preview
-llama-3.2-1b-preview
 llama-3.2-3b-preview
 llama-3.2-90b-text-preview
-llama-guard-3-8b
 llama3-70b-8192
 llama3-8b-8192
 llama3-groq-70b-8192-tool-use-preview
 llama3-groq-8b-8192-tool-use-preview
-mixtral-8x7b-32768
 `
   .trim()
   .split("\n")
