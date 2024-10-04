777a02ae4571a738f45ce0e82f8b318e83014ae2
rednotsus <Jiayangc1@gmail.com>
2024-10-03 23:00:51 -0500

    FINALLY FIX retro bowl

--- index.js
+++ index.js
@@ -42,6 +42,12 @@ function switchModel() {
 
 const server = http.createServer();
 const app = express(server);
+
+app.use("/t/", (req, res, next) => {
+  res.setHeader("X-Frame-Options", "SAMEORIGIN");
+  next();
+});
+
 const bareServer = createBareServer("/t/");
 
 app.use(express.json({ limit: "50mb" }));
@@ -79,7 +85,7 @@ app.post("/api/chat", async (req, res) => {
           Authorization: `Bearer ${process.env.API_KEY}`,
           "Content-Type": "application/json",
         },
-      },
+      }
     );
 
     const aiResponse = response.data.choices[0].message.content;
@@ -134,7 +140,7 @@ app.post("/api/signUp", async (req, res) => {
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
         },
-      },
+      }
     );
 
     if (!captchaVerifyResponse.data.success) {
@@ -154,7 +160,7 @@ app.post("/api/signUp", async (req, res) => {
           Authorization: process.env.workerAUTH,
           "Content-Type": "application/json",
         },
-      },
+      }
     );
 
     res.status(200).json(response.data);
@@ -183,7 +189,7 @@ app.post("/api/login", async (req, res) => {
           Authorization: process.env.workerAUTH,
           "Content-Type": "application/json",
         },
-      },
+      }
     );
 
     res.status(200).json(response.data);
@@ -209,7 +215,7 @@ app.post("/api/checkPremium", async (req, res) => {
           Authorization: process.env.workerAUTH,
           "Content-Type": "application/json",
         },
-      },
+      }
     );
 
     res.status(200).json(response.data);
@@ -237,7 +243,7 @@ app.post("/api/uploadSave", async (req, res) => {
           Authorization: process.env.workerAUTH,
           "Content-Type": "application/json",
         },
-      },
+      }
     );
 
     res.status(200).json(response.data);
@@ -263,7 +269,7 @@ app.post("/api/readSave", async (req, res) => {
           Authorization: process.env.workerAUTH,
           "Content-Type": "application/json",
         },
-      },
+      }
     );
 
     res.status(200).json(response.data);
@@ -306,7 +312,7 @@ async function fetchDataFromGithub(
   res,
   next,
   baseUrl,
-  secondaryUrl = null,
+  secondaryUrl = null
 ) {
   function isAFile(urlString) {
     return urlString.trim().split("/").pop().length !== 0;
