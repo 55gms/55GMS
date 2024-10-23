b6f88840bff77caa744c863dd98c8ae9f6a6672f
RedNotSus <RedNotSus@users.noreply.github.com>
2024-10-23 02:20:55 +0000

    Prettified Code!

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -2,7 +2,7 @@ function script(text) {
   console.log(
     "%cScript Injection",
     "color: cyan; font-weight: 600; background: black; padding: 0 5px; border-radius: 5px",
-    text
+    text,
   );
 }
 
@@ -12,7 +12,7 @@ function script(text) {
 const newScript = document.createElement("script");
 newScript.setAttribute(
   "src",
-  "https://www.googletagmanager.com/gtag/js?id=G-N0LG27M8L8"
+  "https://www.googletagmanager.com/gtag/js?id=G-N0LG27M8L8",
 );
 const inlinegascript = document.createElement("script");
 inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
@@ -59,7 +59,7 @@ fetch("/assets/json/ads.json")
       adscipterz92.setAttribute("async", "");
       adscipterz92.setAttribute(
         "src",
-        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317"
+        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317",
       );
       adscipterz92.setAttribute("crossorigin", "anonymous");
       document.head.append(adscipterz92);
