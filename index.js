64416c3cd1549c1df6f606f43f0e11111598222f
RedNotSus <RedNotSus@users.noreply.github.com>
2025-01-04 04:35:29 +0000

    Prettified Code!

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ app.post("/api/chat", async (req, res) => {
           Authorization: `Bearer ${randomAPIKey}`,
           "Content-Type": "application/json",
         },
-      }
+      },
     );
     if (response.status === 429) {
       switchModel();
@@ -102,7 +102,7 @@ app.post("/api/chat", async (req, res) => {
             Authorization: `Bearer ${randomAPIKey}`,
             "Content-Type": "application/json",
           },
-        }
+        },
       );
       const aiResponse = response.data.choices[0].message.content;
       conversation.push({ role: "assistant", content: aiResponse });
@@ -174,7 +174,7 @@ app.post("/api/signUp", async (req, res) => {
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
         },
-      }
+      },
     );
 
     if (!captchaVerifyResponse.data.success) {
@@ -194,7 +194,7 @@ app.post("/api/signUp", async (req, res) => {
           Authorization: process.env.workerAUTH,
           "Content-Type": "application/json",
         },
-      }
+      },
     );
 
     res.status(200).json(response.data);
@@ -223,7 +223,7 @@ app.post("/api/login", async (req, res) => {
           Authorization: process.env.workerAUTH,
           "Content-Type": "application/json",
         },
-      }
+      },
     );
 
     res.status(200).json(response.data);
@@ -249,7 +249,7 @@ app.post("/api/checkPremium", async (req, res) => {
           Authorization: process.env.workerAUTH,
           "Content-Type": "application/json",
         },
-      }
+      },
     );
 
     res.status(200).json(response.data);
@@ -277,7 +277,7 @@ app.post("/api/uploadSave", async (req, res) => {
           Authorization: process.env.workerAUTH,
           "Content-Type": "application/json",
         },
-      }
+      },
     );
 
     res.status(200).json(response.data);
@@ -303,7 +303,7 @@ app.post("/api/readSave", async (req, res) => {
           Authorization: process.env.workerAUTH,
           "Content-Type": "application/json",
         },
-      }
+      },
     );
 
     res.status(200).json(response.data);
