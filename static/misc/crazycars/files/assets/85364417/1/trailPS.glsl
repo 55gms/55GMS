429d00c01e149e4c5fb2d7e99a66a1d82cfde4b6
rednotsus <Jiayangc1@gmail.com>
2025-01-05 13:16:04 -0600

    crazycars

---
+++ static/misc/crazycars/files/assets/85364417/1/trailPS.glsl
@@ -0,0 +1,13 @@
+varying vec3 vPos;
+varying vec2 vUv;
+varying float vFade;
+
+uniform sampler2D textureD;
+uniform vec3 sourcePos;
+
+void main(void) {
+    float mask = texture2D(textureD, vUv).g;
+    vec3 diff = vPos - sourcePos;
+    gl_FragColor = vec4(0,0,0, mask * vFade * clamp(dot(diff,diff),0.0,1.0));
+}
+
---
+++ static/misc/crazycars/settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1451138.json";
+CONTEXT_OPTIONS = {
+    'antialias': false,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+SCRIPTS = [ 92059935, 40914487, 41015980, 41217562, 41261986, 41334679, 41353004, 51407881, 85439481, 85440691, 85448563, 85725639, 86086490, 86476989, 86728818, 86948388, 87240260, 89044191, 89044217, 89044218, 89044220, 89111191, 89613339, 89643901, 89645735, 89660739, 90170718, 90326092, 90645350, 90844930, 90854157, 90860794, 90892904, 90933266, 91077498, 91233776, 91254353, 91256327, 91258477, 91308134, 91308135, 91308137, 91308138, 91385248, 91385335, 91387750, 91390444, 91391868, 91400175, 91404918, 91415112, 91415547, 91493749, 91899381, 91935109, 92090393, 92207486, 92396819, 92574623, 93573824, 94432075, 94952840, 90857136, 97273109, 97273110, 97273111, 98366456, 100945293, 104225972, 104371667, 104453925, 105165001, 106154795, 107102507, 109133836, 109151980, 109494582 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'DracoDecoderModule', 'glueUrl' : 'files/assets/91308134/1/draco.js', 'wasmUrl' : 'files/assets/91308136/1/draco.wasm.wasm', 'fallbackUrl' : 'files/assets/91308135/1/draco.wasm.js', 'preload' : true},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/40857457/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/40857458/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/40857456/1/ammo.js', 'preload' : true},
+];
