3b266b8743971993cb37b91654c3ce31a6d83933
rednotsus <Jiayangc1@gmail.com>
2025-09-21 23:28:35 -0500

    fix tv shows

--- static/misc/media/embed/js/tv.js
+++ static/misc/media/embed/js/tv.js
@@ -14,5 +14,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
  function play() {
-  location.href = `/embed.html#/https://player.videasy.net/${ID}/${season}/${episode}`;
+  location.href = `/embed.html#https://player.videasy.net/tv/${ID}/${season}/${episode}`;
 }
